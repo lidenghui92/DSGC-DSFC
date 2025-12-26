aee418fee9a3eb72834efab8d2cf1f343cf89552
lidenghui92 <41458088+lidenghui92@users.noreply.github.com>
2025-12-26 18:06:25 +0800

    Update Fig2a.stat.sh

--- COG_L/Fig2a.stat.sh
+++ COG_L/Fig2a.stat.sh
@@ -1,7 +1,7 @@
-zgrep "DSM_" Fig2.COG_L_DS.TS.OM_cluster.row.gz | grep "Soil_" | grep "OM-RGC" | wc -l  #56200
-zgrep "DSM_" Fig2.COG_L_DS.TS.OM_cluster.row.gz | grep "Soil_" | grep -v "OM-RGC" | wc -l  #89186
-zgrep "DSM_" Fig2.COG_L_DS.TS.OM_cluster.row.gz | grep -v "Soil_" | grep  "OM-RGC" | wc -l #19260
-zgrep -v "DSM_" Fig2.COG_L_DS.TS.OM_cluster.row.gz | grep "Soil_" | grep  "OM-RGC" | wc -l #2838
-zgrep "DSM_" Fig2.COG_L_DS.TS.OM_cluster.row.gz | grep -v "Soil_" | grep  -v "OM-RGC" | wc -l  #243218
-zgrep -v "DSM_" Fig2.COG_L_DS.TS.OM_cluster.row.gz | grep "Soil_" | grep  -v "OM-RGC" | wc -l  #297505
-zgrep -v "DSM_" Fig2.COG_L_DS.TS.OM_cluster.row.gz | grep -v "Soil_" | grep  "OM-RGC" | wc -l  #19890
+zgrep "DSM_0" Fig2.COG_L_DS.TS.OM_cluster.row.gz | grep "Soil_" | grep "OM-RGC.v2" | wc -l  #56192
+zgrep "DSM_0" Fig2.COG_L_DS.TS.OM_cluster.row.gz | grep "Soil_" | grep -v "OM-RGC.v2" | wc -l  #89193
+zgrep "DSM_0" Fig2.COG_L_DS.TS.OM_cluster.row.gz | grep -v "Soil_" | grep  "OM-RGC.v2" | wc -l #19260
+zgrep -v "DSM_0" Fig2.COG_L_DS.TS.OM_cluster.row.gz | grep "Soil_" | grep  "OM-RGC.v2" | wc -l #2382
+zgrep "DSM_0" Fig2.COG_L_DS.TS.OM_cluster.row.gz | grep -v "Soil_" | grep  -v "OM-RGC.v2" | wc -l  #243218
+zgrep -v "DSM_0" Fig2.COG_L_DS.TS.OM_cluster.row.gz | grep "Soil_" | grep  -v "OM-RGC.v2" | wc -l  #297962
+zgrep -v "DSM_0" Fig2.COG_L_DS.TS.OM_cluster.row.gz | grep -v "Soil_" | grep  "OM-RGC.v2" | wc -l  #19890
