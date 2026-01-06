a78d170e58c7462d41b5bb1b14264f2497d58bf9
lidenghui92 <41458088+lidenghui92@users.noreply.github.com>
2026-01-06 09:52:00 +0800

    Update Fig2.COG_annotation_by_PC.sh

--- COG_L/Fig2.COG_annotation_by_PC.sh
+++ COG_L/Fig2.COG_annotation_by_PC.sh
@@ -8,15 +8,15 @@ perl -e 'open E,"e5.og_annotations_COGonly.tsv";while(<E>){chomp;@l=split(/\t/,$
 
 ##note:run the script below line-by-line
 
-#cat *_gene2uniqCOG.tsv > all_gene2COG.tsv
+cat *_gene2uniqCOG.tsv > all_gene2COG.tsv
 
-#perl -e 'open C,"all_gene2COG.tsv";while(<C>){@l=split;$c{$l[0]}=$l[1];}open I,"COG_L_PC.member-uid.row";while(<I>){chomp;@l=split;@g=split(/,/,$l[1]);$x=0;print "$l[0]\t";foreach(@g){$x+=1;if(exists $c{$_}){print "$c{$_},";}else{print "NA,";}}print "\t$x\n";}' |sed 's#,\t#\t#g' > COG_L_PC.memberCOG.row
+perl -e 'open C,"all_gene2COG.tsv";while(<C>){@l=split;$c{$l[0]}=$l[1];}open I,"COG_L_PC.member-uid.row";while(<I>){chomp;@l=split;@g=split(/,/,$l[1]);$x=0;print "$l[0]\t";foreach(@g){$x+=1;if(exists $c{$_}){print "$c{$_},";}else{print "NA,";}}print "\t$x\n";}' |sed 's#,\t#\t#g' > COG_L_PC.memberCOG.row
 
-#perl -e 'open I,"COG_L_PC.memberCOG.row";while(<I>){chomp;@l=split;$c{$l[0]}=$l[2];@OG=split(/,/,$l[1]);foreach $og (@OG){if(exists $t{$l[0]}{$og}){$t{$l[0]}{$og}+=1;}else{$t{$l[0]}{$og}=1;}}}foreach $k (keys %t){print "\n$k\t$c{$k}";foreach $ks (sort{$t{$k}{$b} <=>$t{$k}{$a}} keys %{$t{$k}}){$r{$k}{$ks}=$t{$k}{$ks}/$c{$k}*100;print "\t$ks,$t{$k}{$ks},$r{$k}{$ks}%";}}' > COG_L_PC.memberCOG.stat
+perl -e 'open I,"COG_L_PC.memberCOG.row";while(<I>){chomp;@l=split;$c{$l[0]}=$l[2];@OG=split(/,/,$l[1]);foreach $og (@OG){if(exists $t{$l[0]}{$og}){$t{$l[0]}{$og}+=1;}else{$t{$l[0]}{$og}=1;}}}foreach $k (keys %t){print "\n$k\t$c{$k}";foreach $ks (sort{$t{$k}{$b} <=>$t{$k}{$a}} keys %{$t{$k}}){$r{$k}{$ks}=$t{$k}{$ks}/$c{$k}*100;print "\t$ks,$t{$k}{$ks},$r{$k}{$ks}%";}}' > COG_L_PC.memberCOG.stat
 
-#perl -e 'open C,"COG_L_PC.memberCOG.stat";while(<C>){@l=split;@m=split(/,/,$l[2]);$c{$l[0]}=$m[0];}open I,"COG_L_PC.member-uid.row";while(<I>){chomp;@l=split;@g=split(/,/,$l[1]);foreach(@g){print "$_\t$c{$l[0]}\n";}}' > COG_anno_by_PC.tsv
+perl -e 'open C,"COG_L_PC.memberCOG.stat";while(<C>){@l=split;@m=split(/,/,$l[2]);$c{$l[0]}=$m[0];}open I,"COG_L_PC.member-uid.row";while(<I>){chomp;@l=split;@g=split(/,/,$l[1]);foreach(@g){print "$_\t$c{$l[0]}\n";}}' > COG_anno_by_PC.tsv
 
-#fish -bf table -ff table COG_L_PC.member-uid.row COG_anno_by_PC.tsv > rep_COG_anno_by_PC.tsv
+fish -bf table -ff table COG_L_PC.member-uid.row COG_anno_by_PC.tsv > rep_COG_anno_by_PC.tsv
 
 ##note:Statistics of functional consistency within clusters
-#sed 's#,#\t#g' COG_L_PC.memberCOG.stat |sed 's#%##g'|awk -F "\t" '{if($5>=90){a+=1;}if($5>=50){b+=1;}c+=1;}END{print "over 90% members have same COG term in "a" clusters\nover 50% members have same COG term in ",b,"clusters\nWe have ",c," clusters"}'
+sed 's#,#\t#g' COG_L_PC.memberCOG.stat |sed 's#%##g'|awk -F "\t" '{if($5>=90){a+=1;}if($5>=50){b+=1;}c+=1;}END{print "over 90% members have same COG term in "a" clusters\nover 50% members have same COG term in ",b,"clusters\nWe have ",c," clusters"}'
