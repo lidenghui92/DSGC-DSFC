58e8bc3bb989e7a4eabf9366ccf18708815e561d
lidenghui92 <41458088+lidenghui92@users.noreply.github.com>
2026-01-06 15:08:41 +0800

    Update Fig3bc_S3d.2d_density.R

--- StructureCatalog/Fig3bc_S3d.2d_density.R
+++ StructureCatalog/Fig3bc_S3d.2d_density.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(ggplot2)
 library(hexbin)
-data<-read.table("DS-PDB_density.m8",header = F,sep = "\t") #for Fig4. b, use DS-AFDB_density.dat, for Fig4. c, use DS-DS_density.dat
+data<-read.table("DS-AFDB_density.dat",header = F,sep = "\t") #for Fig3. c, use DS-DS_density.dat, for Figs3. d, use DS-PDB_density.dat
 p<-ggplot(data, aes(y=V3, x=V4) ) +
   geom_hex(bins = 300) +
   scale_fill_gradient2(name="count",
@@ -16,4 +16,5 @@ p<-ggplot(data, aes(y=V3, x=V4) ) +
   theme_bw()
 pdf("density_plot.pdf",w=6,h=5)
 p
+
 dev.off()
