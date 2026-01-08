d5a16ae65a23e808526a3e0ed3d7f387ea51c661
GuoYang-qd <122084207+GuoYang-qd@users.noreply.github.com>
2026-01-08 16:49:49 +0800

    Update Fig1fg_S1e.stat.sh

--- GeneCatalog/Fig1fg_S1e.stat.sh
+++ GeneCatalog/Fig1fg_S1e.stat.sh
@@ -12,7 +12,7 @@ grep -v "DSM_0" final_20_50_g2g.list | grep "Soil_" | grep  "OM-RGC.v2" | wc -l 
 grep "DSM_0" final_20_50_g2g.list | grep -v "Soil_" | grep  -v "OM-RGC.v2" | wc -l ####58105982
 grep -v "DSM_0" final_20_50_g2g.list | grep "Soil_" | grep  -v "OM-RGC.v2" | wc -l ####17964801
 grep -v "DSM_0" final_20_50_g2g.list | grep -v "Soil_" | grep  "OM-RGC.v2" | wc -l ####2707314
-###Relevant statistical results for Fig S1F
+###Relevant statistical results for Fig S1e
 grep "," final_20_50_g2g.list | grep "DSM_0" |  grep -v "Soil_" |  grep  -v "OM-RGC.v2" | wc -l   ###17811094
 grep "," final_20_50_g2g.list | grep "Soil_" | grep -v "DSM_0" |  grep  -v "OM-RGC.v2" | wc -l    ###3841389
 grep "," final_20_50_g2g.list | grep "OM-RGC.v2" |  grep -v "Soil_" | grep -v "DSM_0" | wc -l     ###688319
@@ -27,3 +27,4 @@ grep -v "Ocean" DS_GO_20_50_g2g.list | wc -l                ####54551577
 grep -v "DSM_" DS_GO_20_50_g2g.list | wc -l                 ####49141285
 
 grep -v "Ocean" DS_GO_20_50_g2g.list | awk '{for(i=2;i<=NF;i++){print $i}}' | wc -l  ####157133253
+
